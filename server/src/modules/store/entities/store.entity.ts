fe3d86c67d8c95178eec743426855d47d96a6e56
Owais Akbani <owais.akbani92@gmail.com>
2022-12-19 15:37:06 +0500

    modified user service with latest medusa

--- server/src/modules/store/entities/store.entity.ts
+++ server/src/modules/store/entities/store.entity.ts
@@ -2,8 +2,12 @@ import { Store as MedusaStore } from '@medusajs/medusa/dist';
 import { Entity, JoinColumn, OneToMany } from 'typeorm';
 import { Entity as MedusaEntity } from 'medusa-extender';
 
+import { User } from '../../user/entities/user.entity';
+
 @MedusaEntity({ override: MedusaStore })
 @Entity()
 export class Store extends MedusaStore {
-    //TODO add relations
+    @OneToMany(() => User, (user) => user.store)
+    @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+    members: User[];
 }
---
+++ server/src/modules/user/services/user.service.ts
@@ -0,0 +1,53 @@
+import { Service } from 'medusa-extender';
+import { EntityManager } from 'typeorm';
+import EventBusService from '@medusajs/medusa/dist/services/event-bus';
+import { FindConfig } from '@medusajs/medusa/dist/types/common';
+import { UserService as MedusaUserService } from '@medusajs/medusa/dist/services';
+import { User } from '../entities/user.entity';
+import UserRepository from '../repositories/user.repository';
+import { MedusaError } from 'medusa-core-utils';
+
+// import { validateId } from '@medusajs/medusa/dist/utils/validate-id'
+// import { buildQuery } from '@medusajs/medusa/dist/utils/build-query'
+import { AnalyticsConfigService } from '@medusajs/medusa/dist/services';
+import { validateId, buildQuery }from  '@medusajs/medusa/dist/utils'
+import { FlagRouter }from  '@medusajs/medusa/dist/utils/flag-router'
+
+
+type ConstructorParams = {
+    manager: EntityManager;
+    userRepository: typeof UserRepository;
+    eventBusService: EventBusService;
+    analyticsConfigService: AnalyticsConfigService;
+    featureFlagRouter: FlagRouter;
+};
+
+@Service({ override: MedusaUserService })
+export default class UserService extends MedusaUserService {
+    private readonly manager: EntityManager;
+    private readonly userRepository: typeof UserRepository;
+    private readonly eventBus: EventBusService;
+
+
+    constructor(private readonly container: ConstructorParams) {
+        super(container);
+        this.manager = container.manager;
+        this.userRepository = container.userRepository;
+        this.eventBus = container.eventBusService;
+
+    }
+
+    public async retrieve(userId: string, config?: FindConfig<User>): Promise<User> {
+        const userRepo = this.manager.getCustomRepository(this.userRepository);
+        const validatedId = validateId(userId);
+        const query = buildQuery({ id: validatedId }, config);
+
+        const user = await userRepo.findOne(query);
+
+        if (!user) {
+            throw new MedusaError(MedusaError.Types.NOT_FOUND, `User with id: ${userId} was not found`);
+        }
+
+        return user as User;
+    }
+}
